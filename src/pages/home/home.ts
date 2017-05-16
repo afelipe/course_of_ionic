fc710d17c1bf5b5b8d9a8cf74efd05888c1dec6b
AULA 4 <AULA 4>
2017-05-16 18:11:56 +0200

    Add Camera plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
+import {Camera} from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -8,10 +10,28 @@ import { ToastController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  text: string;
+  position: string;
+  public base64Image: string;
+  
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController,
+              private camera:Camera , private geolocation: Geolocation) {
+    console.log('Hello HomeComponent Component');
+    this.text = 'Hello World';
+    this.geolocation.getCurrentPosition().then((resp) => {
+       // resp.coords.latitude
+      // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
 
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+    // data can be a set of coordinates, or an error (if an error occurred).
+    this.position = " "+ data.coords.latitude +" "+ data.coords.longitude;
+    // data.coords.longitude
+    });
   }
-
   showToast(position: string) {
     let toast = this.toastCtrl.create({
       message: 'Se ha guardado la imagen',
@@ -22,12 +42,24 @@ export class HomePage {
     toast.present(toast);
   }
 
-  takePhoto(){
-
-  }
-
   showAlbum(){
     
   }
+
+
+ takePhoto(){
+ 
+   this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+  
 
 }
