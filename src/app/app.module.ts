cc6226ee7b9a88c94fbfe01ad25cd9b60e4a6066
AULA 4 <AULA 4>
2017-05-16 21:48:00 +0200

    comentario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,13 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SlidesPage } from "../pages/home/slides";
 import { ImagePicker } from '@ionic-native/image-picker';
-
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage, SlidesPage
+    HomePage, 
+    SlidesPage
   ],
   imports: [
     BrowserModule,
@@ -24,7 +25,8 @@ import { ImagePicker } from '@ionic-native/image-picker';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,SlidesPage
+    HomePage,
+    SlidesPage
   ],
   providers: [
     StatusBar,
@@ -32,7 +34,8 @@ import { ImagePicker } from '@ionic-native/image-picker';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Camera,
     Geolocation,
-    ImagePicker
+    ImagePicker,
+    SocialSharing
 
   ]
 })
--- src/pages/home/slides.ts
+++ src/pages/home/slides.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { ToastController, ViewController, ActionSheetController, Platform } from 'ionic-angular';
 import { ViewChild } from '@angular/core';
 import { Slides } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'slides',
@@ -32,7 +33,7 @@ export class SlidesPage {
 
     constructor(public navCtrl: NavController, public toastCtrl: ToastController, public vctrl : 
         ViewController, public actionsheetCtrl: ActionSheetController,
-        public platform: Platform) {
+        public platform: Platform, private socialSharing: SocialSharing) {
     }
 
     cerrar(){
@@ -48,10 +49,18 @@ export class SlidesPage {
                 text: 'Facebook',
                 icon: !this.platform.is('ios') ? 'logo-facebook' : null,
                 handler: () => {
-                    let currentIndex = this.slides.getActiveIndex();
-                    console.log("Current index in Facebook", currentIndex);
-                    
-                    //console.log(slidesArray[currentIndex]);
+                    //let currentIndex = this.slides.getActiveIndex();
+                    //console.log("Current index in Facebook", currentIndex);
+                    console.log(this.slidesArray[this.slides.getActiveIndex()].title);
+
+                    let titulo = this.slidesArray[this.slides.getActiveIndex()].title;
+                    let imagen = this.slidesArray[this.slides.getActiveIndex()].image;
+
+                    this.socialSharing.canShareVia("com.facebook.katana","","", "","").then(() => { 
+                        //alert("facebook ok");
+                    }).catch(() => {alert("facebook error");});
+
+                   this.socialSharing.shareViaFacebook(titulo, imagen, "url");
                 }
                 },
                 {
@@ -59,6 +68,16 @@ export class SlidesPage {
                 icon: !this.platform.is('ios') ? 'logo-twitter' : null,
                 handler: () => {
                     console.log('Twitter clicked');
+                    console.log(this.slidesArray[this.slides.getActiveIndex()].title);
+                    
+                    let titulo = this.slidesArray[this.slides.getActiveIndex()].title;
+                    let imagen = this.slidesArray[this.slides.getActiveIndex()].image;
+
+                    this.socialSharing.canShareVia("com.twitter.android","","", "","").then(() => { 
+                        //alert("Twitter ok");
+                    }).catch(() => {alert("Twitter error");});
+
+                    this.socialSharing.shareViaTwitter(titulo, imagen, "url");
                 }
                 },
                 {
@@ -66,6 +85,14 @@ export class SlidesPage {
                 icon: !this.platform.is('ios') ? 'logo-whatsapp' : null,
                 handler: () => {
                     console.log('Whatsapp clicked');
+                    let titulo = this.slidesArray[this.slides.getActiveIndex()].title;
+                    let imagen = this.slidesArray[this.slides.getActiveIndex()].image;
+
+                    this.socialSharing.canShareVia("com.whatsapp","","", "","").then(() => { 
+                        //alert("Whatsapp ok");
+                    }).catch(() => {alert("Whatsapp error");});
+
+                    this.socialSharing.shareViaWhatsApp(titulo, null, null);
                 }
                 
                 }
